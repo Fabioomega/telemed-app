910144d5c455b98e9092adc41da3a37b2e8590af
Fabioomega <fabioms2002@gmail.com>
2025-12-15 12:03:05 -0300

    Added breasts to __init__

--- models/__init__.py
+++ models/__init__.py
@@ -1,9 +1,11 @@
 from .radio.managed_models import create_modality, create_diseases, create_region
 from .ecg.managed_model import create_ecg_classifier
+from .breasts import create_breast
 
 __all__ = [
     "create_modality",
     "create_diseases",
     "create_region",
     "create_ecg_classifier",
+    "create_breast",
 ]

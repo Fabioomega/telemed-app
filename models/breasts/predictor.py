263bf56d5e131c04dcf199aece2bd9efbc94d098
felpzw <lipewinsch@gmail.com>
2025-12-15 11:05:28 -0300

    add numpy list to predictor

--- models/breasts/predictor.py
+++ models/breasts/predictor.py
@@ -1,25 +1,30 @@
 from ultralytics import YOLO
+import numpy as np
 from typing import List, Dict, Any
 
 class BreastDetector:
 
     def __init__(self, model_path: str):
-
         self.model = YOLO(model_path)
 
-    def predict(self, image_path: str, conf: float = 0.5) -> List[Dict[str, Any]]:
+    def predict(self, images: List[np.ndarray], conf: float = 0.5) -> List[List[Dict[str, Any]]]:
 
-        results = self.model.predict(image_path, conf=conf)
+        results = self.model.predict(images, conf=conf)
         
-        detections = []
+        batch_detections = []
+
         for result in results:
+            image_detections = []
+            
             for box in result.boxes:
                 detection = {
                     'class': result.names[int(box.cls)],
                     'confidence': float(box.conf),
-                    #centro x, centro y, largura e altura
+                    # xywh: centro x, centro y, largura, altura
                     'bbox': box.xywh[0].tolist()
                 }
-                detections.append(detection)
+                image_detections.append(detection)
+            
+            batch_detections.append(image_detections)
         
-        return detections
+        return batch_detections
